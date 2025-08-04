c2dcdf500fc1d526a5a493d060410268b25c8ed5
Palex-l <palex.l@gmx.net>
2025-08-04 13:49:52 +0200

    Update pump_trigger.php

--- pump_trigger.php
+++ pump_trigger.php
@@ -1,8 +1,13 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ml'])) {
     $ml = intval($_POST['ml']);
-    if ($ml >= 0) {
-        file_put_contents('pump_queue.txt', $ml);
+    if ($ml >= 0 && $ml <= 1000) {
+        if ($ml == 0) {
+            file_put_contents('pump_queue.txt', "0;0");
+        } else {
+            $timestamp = time();
+            file_put_contents('pump_queue.txt', "$ml;$timestamp");
+        }
         header('Location: index.php?ok=1');
         exit;
     }
