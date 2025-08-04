2fb55d238c08ce08e02cf559772291c47a3c44f2
Palex-l <palex.l@gmx.net>
2025-08-04 13:01:48 +0200

    Update pump_trigger.php

--- pump_trigger.php
+++ pump_trigger.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ml'])) {
     $ml = intval($_POST['ml']);
-    if ($ml > 0 && $ml <= 1000) { // optionaler Grenzwert
+    if ($ml >= 0) {
         file_put_contents('pump_queue.txt', $ml);
         header('Location: index.php?ok=1');
         exit;
